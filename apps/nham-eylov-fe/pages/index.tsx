b9d8eb58f77a0f7b5aef7a7d4b54ca242b89a436
Thenglong Heng <thenglongheng2k@gmail.com>
2022-04-22 18:19:43 +0700

    init project

---
+++ apps/nham-eylov-fe/pages/index.tsx
@@ -0,0 +1,9 @@
+export function Index() {
+  return (
+    <h1 className="text-red-500">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, explicabo.
+    </h1>
+  );
+}
+
+export default Index;
