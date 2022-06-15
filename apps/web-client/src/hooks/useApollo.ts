f27181f0513ac25ea0829e05b8b4b3780ff4bee2
Thenglong Heng <thenglongheng2k@gmail.com>
2022-06-15 07:18:35 +0700

    chore: disable any type for pageProps

--- apps/web-client/src/hooks/useApollo.ts
+++ apps/web-client/src/hooks/useApollo.ts
@@ -2,6 +2,7 @@ import { useMemo } from "react"
 
 import { APOLLO_STATE_PROP_NAME, initializeApollo } from "../graphql/apollo-config"
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const useApollo = (pageProps: any) => {
   const state = pageProps[APOLLO_STATE_PROP_NAME]
   return useMemo(() => initializeApollo(state), [state])
