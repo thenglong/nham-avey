e814425d969669f6fe7ab3882413da8a4c82409e
Thenglong Heng <thenglongheng2k@gmail.com>
2022-06-19 17:03:20 +0700

    feat: init web backoffice

---
+++ apps/web-backoffice/src/app/app.tsx
@@ -0,0 +1,12 @@
+import NxWelcome from "./nx-welcome"
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="web-backoffice" />
+      <div />
+    </>
+  )
+}
+
+export default App
