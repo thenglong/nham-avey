cc96fb80cc34e7e4e2431785aee658c5f187bf0f
Thenglong Heng <thenglongheng2k@gmail.com>
2022-07-10 20:15:14 +0700

    feat: menu icon

--- apps/web-backoffice/src/components/menu-content.tsx
+++ apps/web-backoffice/src/components/menu-content.tsx
@@ -1,5 +1,28 @@
+import { QuestionOutlined, UserOutlined } from "@ant-design/icons"
 import { Menu } from "antd"
+import { ItemType } from "antd/lib/menu/hooks/useItems"
 import { useNavigate } from "react-router-dom"
+
+type LinkItemType = ItemType & {
+  path: string
+}
+
+const items: LinkItemType[] = [
+  {
+    key: "restaurants",
+    title: "Restaurants",
+    label: "Restaurants",
+    path: "/app/restaurants",
+    icon: <QuestionOutlined />,
+  },
+  {
+    key: "users",
+    title: "Restaurants",
+    label: "Users",
+    path: "/app/users",
+    icon: <UserOutlined />,
+  },
+]
 
 const MenuContent = () => {
   const navigate = useNavigate()
@@ -11,14 +34,7 @@ const MenuContent = () => {
       selectedKeys={[""]}
       defaultOpenKeys={[""]}
       className="h-max border-r-0"
-      items={[
-        {
-          key: "restaurants",
-          title: "Restaurants",
-          label: "Restaurants",
-          onClick: () => navigate("/app/restaurants"),
-        },
-      ]}
+      items={items.map(item => ({ ...item, onClick: () => navigate(item.path) }))}
     />
   )
 }
